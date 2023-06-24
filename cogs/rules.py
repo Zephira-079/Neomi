1c685383ded50e30aea3c375ec7ece4ee0166b2f
rcph <rcph110110@gmail.com>
2023-06-24 10:08:10 +0800

    somehumanerrorxd

--- cogs/rules.py
+++ cogs/rules.py
@@ -1,10 +1,13 @@
 from discord.ext import commands
 import discord
 
-class Rules(discord.Cog):
+class Rules(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def nothin(self, ctx):
+        pass
     
 async def setup(bot):
     await bot.add_cog(Rules(bot))
